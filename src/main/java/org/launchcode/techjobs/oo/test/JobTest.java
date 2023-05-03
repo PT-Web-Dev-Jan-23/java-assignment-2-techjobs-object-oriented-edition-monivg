0771a15c4ee1733ed1e578b41cfb64067146a8b2
monivg <monicav2013@gmail.com>
2023-05-02 20:34:31 -0500

    STARTED PART 4  - ERROR WITH EMPLOYER TEST

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,33 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId(){
+        Job job1 = new Job();
+        Job job2 = new Job();
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields(){
+        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(job1 instanceof Job);
+        assertEquals("Product tester", job1.getName());
+        assertEquals(new Employer("ACME"), job1.getEmployer());
+        assertEquals(new Location("Desert"), job1.getLocation());
+        assertEquals(new PositionType("Quality control"), job1.getPositionType());
+        assertEquals(new CoreCompetency("Persistence"),job1.getCoreCompetency());
+    }
 
 }
